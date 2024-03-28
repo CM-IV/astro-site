43cd2bab4ff1b46cebb82bff4d31edb146abbecb
CM-IV <cmathIV@protonmail.com>
2024-03-28 18:39:37 -0500

    Added revoltjs

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,5 +1,10 @@
 import type { APIRoute } from "astro";
 import { Messages, db } from "astro:db";
+// import { Client } from "revolt.js"
+
+
+// const client = new Client();
+// await client.loginBot("0qA-jpNAUyySyM28UVz8nZwPZb4ke9RaqfHVxtBoerZTUjUcn3CbuJ-bK4y53xl-");
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
@@ -17,6 +22,10 @@ export const POST: APIRoute = async ({ request }) => {
   }
   // Do something with the data, then return a success response
   await db.insert(Messages).values({ name, email, content });
+
+  //Send message via Revolt
+  // await client.channels
+  // await client.api.post(`/channels/c4-server/messages`, { content: "You got an email!" });
 
   return new Response(
     JSON.stringify({
