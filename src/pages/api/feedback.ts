4ae25b99ee9a3d15d4e553253fe007a00d6c8e9b
Charlie Mathis IV <charlie@fedora.beast-pc>
2024-04-28 12:49:16 -0500

    astro db

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,17 +1,12 @@
 import type { APIRoute } from "astro";
 import { Messages, db } from "astro:db";
-// import { Client } from "revolt.js"
-
-
-// const client = new Client();
-// await client.loginBot("0qA-jpNAUyySyM28UVz8nZwPZb4ke9RaqfHVxtBoerZTUjUcn3CbuJ-bK4y53xl-");
+import dayjs from "dayjs";
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
   const name = data.get("name") as string;
   const email = data.get("email") as string;
   const content = data.get("message") as string;
-  // Validate the data - you'll probably want to do more than this
   if (!name || !email || !content) {
     return new Response(
       JSON.stringify({
@@ -23,10 +18,6 @@ export const POST: APIRoute = async ({ request }) => {
   // Do something with the data, then return a success response
   await db.insert(Messages).values({ name, email, content });
 
-  //Send message via Revolt
-  // await client.channels
-  // await client.api.post(`/channels/c4-server/messages`, { content: "You got an email!" });
-
   return new Response(
     JSON.stringify({
       message: "Success!",
@@ -34,3 +25,23 @@ export const POST: APIRoute = async ({ request }) => {
     { status: 200 },
   );
 };
+
+export const GET: APIRoute = async () => {
+  // Fetch todays messages from the DB
+  const messages = await db.select().from(Messages);
+
+  const todaysMessages = messages.filter((message) => {
+    const messageDate = dayjs(message.date);
+    const today = dayjs();
+    return messageDate.isSame(today, 'day');
+  });
+
+  return new Response(
+    JSON.stringify(todaysMessages), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  )
+}
