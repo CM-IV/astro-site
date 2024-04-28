fc1e7c50d2d9b2fb664de94dbf32799607d47e58
Charlie Mathis IV <charlie@fedora.beast-pc>
2024-04-28 13:18:29 -0500

    Rework solidjs types

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,4 +1,4 @@
-import { createSignal, createResource } from "solid-js";
+import { createSignal, createResource, Show } from "solid-js";
 
 
 async function postFormData(formData: FormData) {
@@ -22,49 +22,56 @@ const ContactForm = () => {
   }
 
   return (
-    <form onSubmit={submit} id="form">
-      <input type="hidden" />
-      <input
-        type="checkbox"
-        class="hidden"
-        style="display:none"
-        name="botcheck"
-      />
-      <div class="mb-5">
+    <>
+      <form onSubmit={submit} id="form">
+        <input type="hidden" />
         <input
-          type="text"
-          placeholder="Full Name"
-          required
-          class="w-full px-4 py-3 border-2 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
-          name="name"
+          type="checkbox"
+          class="hidden"
+          style="display:none"
+          name="botcheck"
         />
-      </div>
-      <div class="mb-5">
-        <label for="email_address" class="sr-only">
-          Email Address
-        </label>
-        <input
-          id="email_address"
-          type="email"
-          placeholder="Email Address"
-          name="email"
-          required
-          class="w-full px-4 py-3 border-2 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
-        />
-      </div>
-      <div class="mb-3">
-        <textarea
-          name="message"
-          required
-          placeholder="Your Message"
-          class="w-full px-4 py-3 border-2 rounded-md outline-none h-36 focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"></textarea>
-      </div>
-      <button class={"bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"} type="submit" size="lg">
-        Send Message
-      </button>
-      <br />
-      {response() && <p>{response().message}</p>}
-    </form>
+        <div class="mb-5">
+          <input
+            type="text"
+            placeholder="Full Name"
+            required
+            class="w-full px-4 py-3 border-2 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
+            name="name"
+          />
+        </div>
+        <div class="mb-5">
+          <label for="email_address" class="sr-only">
+            Email Address
+          </label>
+          <input
+            id="email_address"
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+            class="w-full px-4 py-3 border-2 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
+          />
+        </div>
+        <div class="mb-3">
+          <textarea
+            name="message"
+            required
+            placeholder="Your Message"
+            class="w-full px-4 py-3 border-2 rounded-md outline-none h-36 focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"></textarea>
+        </div>
+        <button class={"bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"} type="submit">
+          Send Message
+        </button>
+        <br />
+      </form>
+      <Show when={!response.loading} fallback={<div>Loading...</div>}>
+        <p>{response().message}</p>
+      </Show>
+      <Show when={response.error}>
+        <p>There was an error!</p>
+      </Show>
+    </>
   );
 };
 
